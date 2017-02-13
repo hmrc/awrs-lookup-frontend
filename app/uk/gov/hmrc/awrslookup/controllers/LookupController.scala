d4ad8bdd12fb1d4b991baee3846ac433399c0b48
JoelTurner <joel.turner@digital.hmrc.gov.uk>
2017-02-13 15:14:51 +0000

    Started refactor

--- app/uk/gov/hmrc/awrslookup/controllers/LookupController.scala
+++ app/uk/gov/hmrc/awrslookup/controllers/LookupController.scala
@@ -43,21 +43,27 @@ class LookupController @Inject()(val environment: Environment,
 
   private[controllers] def validateFormAndSearch(preValidationForm: PrevalidationAPI[Query], action: Call, lookupCall: lookupServiceCall, fromMulti: Boolean, originalSearchTerm: Option[String])(implicit request: Request[AnyContent]): Future[Result] = preValidationForm.bindFromRequest.fold(
     formWithErrors => {
-//        val queryString = preValidationForm.
-//      println("****************************** preValidationForm.form.get.query: "+preValidationForm.form.get.query)
-//        lookupCall(queryString) map {
-//          case None | Some(SearchResult(Nil)) => Ok(views.html.lookup.search_main(preValidationForm.form, action, searchTerm = queryString, searchResult = SearchResult(Nil)))
-//          case (Some(result@SearchResult(list))) if list.size > 1 => Ok(views.html.lookup.search_main(searchForm.form, action, searchTerm = queryString, searchResult = result))
-//          case Some(r: SearchResult) => Ok(views.html.lookup.single_result(searchForm.form, action, r.results.head, searchTerm = queryString, fromMulti = fromMulti, originalSearchTerm = originalSearchTerm))
-//        }
-//      },
-//
-                  println("**** formWithErrors: "+formWithErrors)
-      Ok(views.html.lookup.search_main(formWithErrors, action))},
+      //        val queryString = preValidationForm.
+      //      println("****************************** preValidationForm.form.get.query: "+preValidationForm.form.get.query)
+      //        lookupCall(queryString) map {
+      //          case None | Some(SearchResult(Nil)) => Ok(views.html.lookup.search_main(preValidationForm.form, action, searchTerm = queryString, searchResult = SearchResult(Nil)))
+      //          case (Some(result@SearchResult(list))) if list.size > 1 => Ok(views.html.lookup.search_main(searchForm.form, action, searchTerm = queryString, searchResult = result))
+      //          case Some(r: SearchResult) => Ok(views.html.lookup.single_result(searchForm.form, action, r.results.head, searchTerm = queryString, fromMulti = fromMulti, originalSearchTerm = originalSearchTerm))
+      //        }
+      //      },
+      //
+      println("\n\n\nFROM MULTI:::"+fromMulti)
+      fromMulti match {
+        case true =>
+          Ok(views.html.lookup.search_main(formWithErrors, action, searchTerm = "", searchResult = None))
+        case _ =>
+          Ok(views.html.lookup.search_main(formWithErrors, action))
+      }
+    },
     queryForm => {
       val queryString = queryForm.query
       lookupCall(queryString) map {
-        case None | Some(SearchResult(Nil)) => Ok(views.html.lookup.search_main(preValidationForm.form, action, searchTerm = queryString, searchResult = SearchResult(Nil)))
+        case None | Some(SearchResult(Nil)) => Ok(views.html.lookup.search_no_results(preValidationForm.form, action, searchTerm = queryString, searchResult = SearchResult(Nil)))
         case (Some(result@SearchResult(list))) if list.size > 1 => Ok(views.html.lookup.search_main(searchForm.form, action, searchTerm = queryString, searchResult = result))
         case Some(r: SearchResult) => Ok(views.html.lookup.single_result(searchForm.form, action, r.results.head, searchTerm = queryString, fromMulti = fromMulti, originalSearchTerm = originalSearchTerm))
       }
