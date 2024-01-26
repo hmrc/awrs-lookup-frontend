17007a58d5737839f41f9be73d454950638fab25
Ben Ryan <37295698+benryan1790@users.noreply.github.com>
2024-01-26 08:50:34 +0000

    Final dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,15 +4,13 @@ object AppDependencies {
   import play.core.PlayVersion
   import play.sbt.PlayImport._
 
-  private val playPartialsVersion      = "9.1.0"
-  private val scalaTestPlusPlayVersion = "5.1.0"
-  private val mockitoVersion           = "1.10.19"
+  private val bootstrapVersion         = "8.4.0"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % "8.0.0",
-    "uk.gov.hmrc" %% "play-partials-play-30"      % playPartialsVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.0.0"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % "8.4.0",
+    "uk.gov.hmrc" %% "play-partials-play-30"      % "9.1.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % bootstrapVersion
   )
 
   trait TestDependencies {
@@ -23,10 +21,10 @@ object AppDependencies {
   object Test {
     def apply(): Seq[sbt.ModuleID] = new TestDependencies {
       override lazy val test = Seq(
-        "uk.gov.hmrc"            %% "bootstrap-test-play-30" % "8.0.0"                 % scope,
-        "org.jsoup"              %  "jsoup"                  % "1.16.2"                 % scope,
-        "org.mockito"            %  "mockito-all"            % mockitoVersion           % scope,
-        "org.mockito"            %  "mockito-core"           % "5.7.0"                  % scope,
+        "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion         % scope,
+        "org.jsoup"              %  "jsoup"                  % "1.17.2"                 % scope,
+        "org.mockito"            %  "mockito-all"            % "1.10.19"                % scope,
+        "org.mockito"            %  "mockito-core"           % "5.10.0"                 % scope,
         "org.scalatestplus"      %% "mockito-4-11"           % "3.2.17.0"               % scope
       )
     }.test
