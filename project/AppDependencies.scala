ba853df84176277ddc36b98f3aad49c7488016b3
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2024-01-30 09:35:07 +0000

    DL-12446 use defined version parameter for bootstrap fe

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % "8.4.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc" %% "play-partials-play-30"      % "9.1.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % bootstrapVersion
   )
