7f435ddfb3d79d39314236555d243d5ccba3530a
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2024-01-30 09:58:41 +0000

    DL-12446 remove unused test dependency and tidy

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -21,11 +21,10 @@ object AppDependencies {
   object Test {
     def apply(): Seq[sbt.ModuleID] = new TestDependencies {
       override lazy val test = Seq(
-        "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion         % scope,
-        "org.jsoup"              %  "jsoup"                  % "1.17.2"                 % scope,
-        "org.mockito"            %  "mockito-all"            % "1.10.19"                % scope,
-        "org.mockito"            %  "mockito-core"           % "5.10.0"                 % scope,
-        "org.scalatestplus"      %% "mockito-4-11"           % "3.2.17.0"               % scope
+        "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % scope,
+        "org.jsoup"   %  "jsoup"                  % "1.17.2"         % scope,
+        "org.mockito" %  "mockito-all"            % "1.10.19"        % scope,
+        "org.mockito" %  "mockito-core"           % "5.10.0"         % scope
       )
     }.test
   }
