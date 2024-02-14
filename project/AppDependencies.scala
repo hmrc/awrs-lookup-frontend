791c0103850f13872aea5adc3d3ddf8cb428185a
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2024-02-14 14:33:18 +0000

    newCrown update dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,16 +1,16 @@
 import sbt._
 
 object AppDependencies {
-  import play.core.PlayVersion
   import play.sbt.PlayImport._
 
   private val bootstrapVersion         = "8.4.0"
+  private val playVersion              = "8.5.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc" %% "play-partials-play-30"      % "9.1.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % bootstrapVersion
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % playVersion
   )
 
   trait TestDependencies {
@@ -20,7 +20,7 @@ object AppDependencies {
 
   object Test {
     def apply(): Seq[sbt.ModuleID] = new TestDependencies {
-      override lazy val test = Seq(
+      override lazy val test: Seq[sbt.ModuleID] = Seq(
         "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % scope,
         "org.jsoup"   %  "jsoup"                  % "1.17.2"         % scope,
         "org.mockito" %  "mockito-all"            % "1.10.19"        % scope,
