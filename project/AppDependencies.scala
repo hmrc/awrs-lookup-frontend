be68fe0937604c16e9003449f9aabfed2a4b4eb3
Byasar3 <byasar3@users.noreply.github.com>
2025-06-25 15:15:58 +0100

    Dependency Update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,20 +19,20 @@ import sbt.*
 object AppDependencies {
   import play.sbt.PlayImport.*
 
-  private val bootstrapVersion         = "9.11.0"
-  private val playVersion              = "11.12.0"
+  private val bootstrapVersion         = "9.13.0"
+  private val playVersion              = "12.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-partials-play-30"      % "10.0.0",
+    "uk.gov.hmrc" %% "play-partials-play-30"      % "10.1.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % playVersion
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "org.jsoup"   %  "jsoup"                  % "1.19.1"         % Test,
-    "org.mockito" %  "mockito-core"           % "5.16.1"         % Test
+    "org.jsoup"   %  "jsoup"                  % "1.21.1"         % Test,
+    "org.mockito" %  "mockito-core"           % "5.18.0"         % Test
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
